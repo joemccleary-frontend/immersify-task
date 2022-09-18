15bce8e54fd49695b359ef7c523af3288036e733
Joe McCleary <30754844+joemccleary-frontend@users.noreply.github.com>
2022-09-18 10:41:31 +0000

    Created new ladybugFull object
    Created new object with ladybug appended to name, description and image name. Object image path updated to png

--- Landing.js
+++ Landing.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { GetLadybugs } from '../Utils/getLadybugs'
 import { DisplayContent } from './DisplayContent/DisplayContent'
 var ladybugNames = [];
+var ladybugFull = [];
 var ladybugDescription = [];
 var ladybugImage = [];
 
@@ -12,20 +13,24 @@ export const Landing = () => {
 
     function gotDataCallback (data) {
         var jsonData = data();
-        var objectData = JSON.parse(jsonData)
+        var objectData = JSON.parse(jsonData);
 
         objectData.ladybugs.forEach(bug => {
             if (bug.name) {
-                ladybugNames.push(bug.name)
-            }
-            if (bug.description) {
-                ladybugDescription.push(bug.description)
+                ladybugNames.push(bug.name);
+                ladybugFull[bug.name] = {};
+                ladybugFull[bug.name].a = bug.name+' ladybug';
             }
             if (bug.imageLocation) {
-                ladybugImage.push(bug.imageLocation)
+                ladybugImage.push(bug.imageLocation);
+                ladybugFull[bug.name].b = bug.imageLocation.replace("jpg","png");
             }
-            
+            if (bug.description) {
+                ladybugDescription.push(bug.description);
+                ladybugFull[bug.name].c = bug.description;
+            }            
         });
+        console.log(ladybugFull);
 
     }
 
